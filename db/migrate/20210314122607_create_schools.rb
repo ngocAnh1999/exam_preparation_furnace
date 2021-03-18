648511e09203d297b94d439d4b9b2a54e05495c3
oHoVietTuan <ho.viet.tuan@sun-asterisk.com>
2021-03-18 16:40:55 +0700

    add template

--- db/migrate/20210314122607_create_schools.rb
+++
@@ -1,8 +0,0 @@
-class CreateSchools < ActiveRecord::Migration[6.0]
-  def change
-    create_table :schools do |t|
-      t.string :name, null: false
-      t.string :location, null: false
-    end
-  end
-end
