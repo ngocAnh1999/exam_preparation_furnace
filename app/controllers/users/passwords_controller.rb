c1ecc934655e99a4e725e0aab4f8c443d11f1c5e
ngocAnh1999 <anhshmily2512@gmail.com>
2021-03-22 14:09:24 +0700

    add template view teacher list created test

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,8 @@
 class Users::PasswordsController < Devise::PasswordsController
   protected
   def after_sending_reset_password_instructions_path_for resource_name
+    return teachers_root_path if resource.Teacher?
+
     root_path
   end
 end
