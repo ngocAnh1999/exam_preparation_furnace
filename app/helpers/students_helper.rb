11214db275e0df3937d9ee723b4205838e9d4a14
ngocAnh1999 <anhshmily2512@gmail.com>
2021-04-19 23:56:29 +0700

    commit some change

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -3,7 +3,6 @@ module StudentsHelper
     return task.test_doing_time*60 unless task.started_at?
 
     timer = (task.started_at + task.test_doing_time.minutes - Time.zone.now).to_i
-
     return timer if timer > 0
 
     0
